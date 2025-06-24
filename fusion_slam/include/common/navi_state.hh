60974bad7d9f903e3729cdc2efa467846a7a8fcf
lihang <1019825699@qq.com>
2025-06-25 00:46:43 +0800

    "增加了第三方的sophus库"

---
+++ fusion_slam/include/common/navi_state.hh
@@ -0,0 +1,32 @@
+#pragma once
+#include "eigen_type.hh"
+#include "sophus/so3.hpp"
+
+namespace slam {
+struct NaviState {
+    NaviState() = default;
+
+    // from time, R, p, v, bg, bad
+    explicit NaviState(double time, const SO3& R = SO3(), const Vec3d& t = Vec3d::Zero(),
+                       const Vec3d& v = Vec3d::Zero(), const Vec3d& bg = Vec3d::Zero(), const Vec3d& ba = Vec3d::Zero())
+        : timestamp_(time), R_(R), p_(t), v_(v), bg_(bg), ba_(ba) {
+    }
+
+    // from pose and vel
+    NaviState(double time, const SE3& pose, const Vec3d& vel = Vec3d::Zero())
+        : timestamp_(time), R_(pose.so3()), p_(pose.translation()), v_(vel) {
+    }
+    /// 转换到Sophus
+    Sophus::SE3d GetSE3() const {
+        return Sophus::SE3d(R_, p_);
+    }
+
+    double timestamp_;
+    Vec3d p_ = Vec3d::Zero();
+    Vec3d v_ = Vec3d::Zero();
+    SO3 R_;
+    Vec3d bg_ = Vec3d::Zero();
+    Vec3d ba_ = Vec3d::Zero();
+    Vec3d gravity_ = Vec3d::Zero();
+};
+}  // namespace slam
---
+++ fusion_slam/include/lio-ieskf/ieskf.hh
@@ -0,0 +1,19 @@
+/*
+ * @Author: lihang 1019825699@qq.com
+ * @Date: 2025-06-24 23:36:17
+ * @LastEditors: lihang 1019825699@qq.com
+ * @LastEditTime: 2025-06-25 00:40:04
+ * @FilePath: /fusion_slam_ws/src/fusion_slam/include/lio-ieskf/ieskf.hh
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置:
+ * https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AEam
+ */
+#pragma once
+
+#include "common/eigen_type.hh"
+
+namespace slam {
+class IESKF {
+   public:
+   private:
+};
+}  // namespace slam
