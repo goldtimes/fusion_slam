9ea880b1dacb5fa074af188691701f3aa789758a
lihang <lihang@kilox.cn>
2025-07-01 08:25:02 +0000

    "通过编译"

--- fusion_slam/src/lio_node.cc
+++ fusion_slam/src/lio_node.cc
@@ -1,4 +1,3 @@
-#include <livox_ros_driver/CustomMsg.h>
 #include <nav_msgs/Odometry.h>
 #include <nav_msgs/Path.h>
 #include <ros/ros.h>
@@ -6,11 +5,13 @@
 #include <sensor_msgs/PointCloud2.h>
 #include <tf/transform_broadcaster.h>
 #include <yaml-cpp/yaml.h>
+#include <mutex>
 #include <thread>
 #include "SystemConfig.hh"
 #include "common/eigen_type.hh"
 #include "common/logger.hpp"
 #include "common_lib.hh"
+#include "livox_ros_driver/CustomMsg.h"
 #include "static_imu_init.hh"
 
 namespace slam {
@@ -60,6 +61,49 @@ class LIONode {
     void ImuCallback(const sensor_msgs::Imu::ConstPtr& imu_msg);
     void StandardLidarCallback(const sensor_msgs::PointCloud2::ConstPtr& cloud_msg);
     void LivoxLidarCallback(const livox_ros_driver::CustomMsg::ConstPtr& livox_msg);
+    void rosIMUtoIMU(const sensor_msgs::Imu::ConstPtr& imu_msgs, IMUData& imu_data, bool is_livox = false,
+                     bool has_orientation = false) {
+        imu_data.timestamped_ = imu_msgs->header.stamp.toSec();
+        if (is_livox) {
+            const double gravity = 9.81;
+            imu_data.acc_ =
+                Eigen::Vector3d(imu_msgs->linear_acceleration.x * 9.81, imu_msgs->linear_acceleration.y * 9.81,
+                                imu_msgs->linear_acceleration.z * 9.81);
+        }
+        imu_data.gyro_ =
+            Eigen::Vector3d(imu_msgs->angular_velocity.x, imu_msgs->angular_velocity.y, imu_msgs->angular_velocity.z);
+        if (has_orientation) {
+            imu_data.orientation.w() = imu_msgs->orientation.w;
+            imu_data.orientation.x() = imu_msgs->orientation.x;
+            imu_data.orientation.y() = imu_msgs->orientation.y;
+            imu_data.orientation.z() = imu_msgs->orientation.z;
+        }
+    }
+
+    PointCloud::Ptr livox2pcl(const livox_ros_driver::CustomMsg::ConstPtr& livox_msg) {
+        PointCloudPtr cloud(new PointCloud);
+        const double min_dist = m_node_config.lidar_min_range * m_node_config.lidar_min_range;
+        const double max_dist = m_node_config.lidar_max_range * m_node_config.lidar_max_range;
+        int point_num = livox_msg->point_num;
+        cloud->reserve(point_num / m_node_config.lidar_filter_num + 1);
+        for (int i = 0; i < point_num; i += m_node_config.lidar_filter_num) {
+            if ((livox_msg->points[i].line < 4) &&
+                ((livox_msg->points[i].tag & 0x30) == 0x10 || (livox_msg->points[i].tag & 0x30) == 0x00)) {
+                float x = livox_msg->points[i].x;
+                float y = livox_msg->points[i].y;
+                float z = livox_msg->points[i].z;
+                if (x * x + y * y + z * z < min_dist || x * x + y * y + z * z > max_dist) continue;
+                PointXYZIRT p;
+                p.x = x;
+                p.y = y;
+                p.z = z;
+                p.intensity = livox_msg->points[i].reflectivity;
+                p.time = livox_msg->points[i].offset_time / 1000000.0f;
+                cloud->push_back(p);
+            }
+        }
+        return cloud;
+    }
 
    private:
     ros::NodeHandle nh_;
@@ -77,6 +121,15 @@ class LIONode {
     LIONodeConfig m_node_config;
     // thead
     std::thread sync_thread_;
+
+    std::mutex imu_mutex_;
+    std::mutex lidar_mutex_;
+    std::deque<IMUData> imu_queue_;
+    std::deque<PointCloud::Ptr> lidar_queue_;
+    std::deque<double> lidar_time_queue_;
+
+    double last_imu_time;
+    double last_lidar_time;
 };
 }  // namespace slam
 
@@ -136,6 +189,42 @@ void slam::LIONode::loadParames() {
     m_node_config.r_il << r_il_vec[0], r_il_vec[1], r_il_vec[2], r_il_vec[3], r_il_vec[4], r_il_vec[5], r_il_vec[6],
         r_il_vec[7], r_il_vec[8];
     m_node_config.lidar_cov_inv = config["lidar_cov_inv"].as<double>();
+}
+
+void slam::LIONode::ImuCallback(const sensor_msgs::Imu::ConstPtr& imu_msg) {
+    std::lock_guard<std::mutex> lck(imu_mutex_);
+    IMUData imu_data;
+    double curr_imu_time = imu_msg->header.stamp.toSec();
+    if (curr_imu_time < last_imu_time) {
+        LOG_INFO("Detected imu loop back");
+        imu_queue_.clear();
+    }
+    if (m_node_config.imu_topic == "/livox/imu") {
+        rosIMUtoIMU(imu_msg, imu_data, true, false);
+    } else {
+        rosIMUtoIMU(imu_msg, imu_data, false, false);
+    }
+    imu_queue_.push_back(imu_data);
+    last_imu_time = curr_imu_time;
+}
+void slam::LIONode::StandardLidarCallback(const sensor_msgs::PointCloud2::ConstPtr& cloud_msg) {
+}
+void slam::LIONode::LivoxLidarCallback(const livox_ros_driver::CustomMsg::ConstPtr& livox_msg) {
+    LOG_INFO("LivoxLidarCallback");
+    PointCloudPtr cloud_ptr = livox2pcl(livox_msg);
+    std::lock_guard<std::mutex> lck(lidar_mutex_);
+    double current_lidar_time = livox_msg->header.stamp.toSec();
+    if (current_lidar_time < last_lidar_time) {
+        LOG_INFO("Detected lidar loop back");
+        lidar_queue_.clear();
+        lidar_time_queue_.clear();
+    }
+    lidar_queue_.push_back(cloud_ptr);
+    lidar_time_queue_.push_back(current_lidar_time);
+    last_lidar_time = current_lidar_time;
+}
+
+void slam::LIONode::run() {
 }
 
 int main(int argc, char** argv) {
