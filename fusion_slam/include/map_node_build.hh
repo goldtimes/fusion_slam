bc0bae2d5634defadfa80d0f43527cfe5f154e20
lihang <lihang@kilox.cn>
2025-07-08 20:41:22 +0800

    "add map build"

--- fusion_slam/include/map_node_build.hh
+++ fusion_slam/include/map_node_build.hh
@@ -3,6 +3,7 @@
 #include <tf2_ros/transform_broadcaster.h>
 #include "common/common_lib.hh"
 #include "common/logger.hh"
+#include "lidar_process.hh"
 
 namespace slam {
 class MapBuildNode {
@@ -16,6 +17,7 @@ class MapBuildNode {
    private:
     void imu_callback(const sensor_msgs::Imu::ConstPtr& imu_msg);
     void standard_pcl_callback(const sensor_msgs::PointCloud2::ConstPtr& lidar_msg);
+    void livox_callback(const livox_ros_driver2::CustomMsg::ConstPtr& lidar_msg);
     void encoder_callback(const nav_msgs::Odometry::ConstPtr& odom_msg);
     void gnss_callback(const sensor_msgs::NavSatFix::ConstPtr& gnss_msg);
 
@@ -25,10 +27,26 @@ class MapBuildNode {
    private:
     ros::NodeHandle nh_;
     MapBuildNodeConfig config_;
+    LidarProcess::LidarProcessConfig lidar_process_config_;
     tf2_ros::TransformBroadcaster tf_broadcaster_;
     ros::Subscriber imu_sub_;
     ros::Subscriber lidar_sub_;
     ros::Subscriber gnss_sub_;
     ros::Subscriber encoder_sub_;
+
+    std::deque<PointCloudPtr> lidar_queque_;
+    std::deque<double> lidar_time_queue_;
+    std::deque<IMUData> imu_queque_;
+    std::deque<Encoder> encoder_queque_;
+    std::deque<GNSS> gnss_queque_;
+    //
+    double last_imu_time = -1;
+    double last_lidar_time = -1;
+    double last_encoder_time = -1;
+    double last_gnss_time = -1;
+
+    std::shared_ptr<LidarProcess> lidar_process_ptr_;
+
+    std::mutex mtx;
 };
 }  // namespace slam
--- fusion_slam/src/map_node_build.cc
+++ fusion_slam/src/map_node_build.cc
@@ -5,13 +5,47 @@ MapBuildNode::MapBuildNode(const ros::NodeHandle& nh) : nh_(nh) {
     LOG_INFO("MapBuildNode Initing.....");
     load_params();
     init_sub_pub();
+    lidar_process_ptr_ = std::make_shared<LidarProcess>();
     LOG_INFO("MapBuildNode initied.....");
 }
 
 void MapBuildNode::imu_callback(const sensor_msgs::Imu::ConstPtr& imu_msg) {
+    std::lock_guard<std::mutex> lck(mtx);
+    double current_imu_time = imu_msg->header.stamp.toSec();
+    if (current_imu_time < last_imu_time) {
+        LOG_INFO("IMU detected loop, last_imu_time:{}, current_time:{}", last_imu_time, current_imu_time);
+        imu_queque_.clear();
+    }
+    last_imu_time = current_imu_time;
+    IMUData imu_data(imu_msg);
+    imu_queque_.push_back(imu_data);
 }
 void MapBuildNode::standard_pcl_callback(const sensor_msgs::PointCloud2::ConstPtr& lidar_msg) {
+    std::lock_guard<std::mutex> lck(mtx);
+    double current_lidar_time = lidar_msg->header.stamp.toSec();
+    if (current_lidar_time < last_lidar_time) {
+        LOG_INFO("lidar detected loop, last_imu_time:{}, current_time:{}", last_lidar_time, current_lidar_time);
+        lidar_queque_.clear();
+    }
+    PointCloudPtr current_cloud(new PointCloud);
+    lidar_process_ptr_->Process(lidar_msg, current_cloud);
+    lidar_queque_.push_back(current_cloud);
+    lidar_time_queue_.push_back(current_lidar_time);
 }
+
+void MapBuildNode::livox_callback(const livox_ros_driver2::CustomMsg::ConstPtr& lidar_msg) {
+    std::lock_guard<std::mutex> lck(mtx);
+    double current_lidar_time = lidar_msg->header.stamp.toSec();
+    if (current_lidar_time < last_lidar_time) {
+        LOG_INFO("lidar detected loop, last_imu_time:{}, current_time:{}", last_lidar_time, current_lidar_time);
+        lidar_queque_.clear();
+    }
+    PointCloudPtr current_cloud(new PointCloud);
+    lidar_process_ptr_->Process(lidar_msg, current_cloud);
+    lidar_queque_.push_back(current_cloud);
+    lidar_time_queue_.push_back(current_lidar_time);
+}
+
 void MapBuildNode::encoder_callback(const nav_msgs::Odometry::ConstPtr& odom_msg) {
 }
 void MapBuildNode::gnss_callback(const sensor_msgs::NavSatFix::ConstPtr& gnss_msg) {
